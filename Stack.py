8a09cd69bea851b1c9aa1702e7cd94ef3d5308eb
Your NameTomRiej <Tom8434@dubaicollege.org>
2020-10-01 12:26:13 +0400

    Create Stack.py
    V1 basic framework

---
+++ Stack.py
@@ -0,0 +1,32 @@
+# Tom Rietjens
+# stack Data Structure
+
+
+class Stack:
+    def __init__(self, maxSize):
+        self.maxSize = maxSize
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def isFull(self):
+        pass
+
+    def isEmpty(self):
+        pass
+
+
+def main():
+    mystack = Stack(5)
+    print(mystack.maxSize)
+
+
+    
+if __name__ == "__main__":
+    main()
